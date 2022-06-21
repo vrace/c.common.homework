85d606ec90ee2acf405bfb74aa8aa2827bb03d14
Yang <vrace_studios@hotmail.com>
2022-06-21 23:02:53 +0800

    add feature - add max guess to guess number game

--- 猜数字游戏.c
+++ 猜数字游戏.c
@@ -2,28 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
+static const int RANGE = 100;
+static const int MAX_GUESS = 7;
+
 int main(void)
 {
     int answer;
     int input;
-    int range = 100;
+    int i;
     
     srand((unsigned int)time(NULL));
-    answer = rand() % range;
+    answer = rand() % RANGE;
 
-    for (;;) {
-        printf("Guess the number: (0 ~ %d): ", range);
+    for (i = 0; i < MAX_GUESS; i++) {
+        printf("Guess the number: (0 ~ %d): ", RANGE);
         scanf("%d", &input);
         if (input < answer) {
-            printf("Too small!\nTry again! ");
+            printf("Too small!\n");
         }
         else if (input > answer) {
-            printf("Too large!\nTry again! ");
+            printf("Too large!\n");
         }
         else {
             printf("Correct answer!\n");
             break;
         }
+    }
+
+    if (i == MAX_GUESS) {
+        printf("Ran out of %d chances!\n", MAX_GUESS);
+        printf("The actual answer is %d\n", answer);
     }
 
     return 0;
