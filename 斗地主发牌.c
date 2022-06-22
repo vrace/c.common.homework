1462fdf36b15066976f73efce166a188f44231ab
Yang <vrace_studios@hotmail.com>
2022-06-22 23:10:27 +0800

    shuffle program - use better card type

--- 斗地主发牌.c
+++ 斗地主发牌.c
@@ -3,9 +3,19 @@
 #include <string.h>
 #include <time.h>
 
+typedef enum card_type
+{
+    CARD_TYPE_HEART,
+    CARD_TYPE_SPADE,
+    CARD_TYPE_DIAMOND,
+    CARD_TYPE_CLUB,
+    CARD_TYPE_JOKER,
+} CARD_TYPE;
+
 typedef struct card
 {
-    char name[20];
+    CARD_TYPE type;
+    int index;
 } CARD;
 
 typedef struct deck
@@ -17,19 +27,20 @@ void init_deck(DECK *deck)
 {
     int i, k;
     CARD *card = &deck->cards[0];
-    const char *type[] = {"HEART", "SPADE", "DIAMOND", "CLUB"};
-    const char *seq[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    const CARD_TYPE types[] = {CARD_TYPE_HEART, CARD_TYPE_SPADE, CARD_TYPE_DIAMOND, CARD_TYPE_CLUB};
 
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < sizeof(types) / sizeof(types[0]); i++) {
         for (k = 0; k < 13; k++) {
-            strcpy(card->name, type[i]);
-            strcat(card->name, " ");
-            strcat(card->name, seq[k]);
-            ++card;
+            card->type = types[i];
+            card->index = k;
+            card++;
         }
     }
-    strcpy(card++->name, "RED JOKER");
-    strcpy(card++->name, "BLACK JOKER");
+    card->type = CARD_TYPE_JOKER;
+    card->index = 0;
+    card++;
+    card->type = CARD_TYPE_JOKER;
+    card->index = 1;
 }
 
 void shuffle_deck(DECK *deck)
@@ -47,6 +58,29 @@ void shuffle_deck(DECK *deck)
     }
 }
 
+void draw_card(CARD *card)
+{
+    static const char *types[] = {"HEART", "SPADE", "DIAMOND", "CLUB"};
+    static const char *indices[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    static const char *jokers[] = {"RED JOKER", "BLACK JOKER"};
+    char buf[13];
+
+    switch (card->type) {
+        case CARD_TYPE_HEART:
+        case CARD_TYPE_SPADE:
+        case CARD_TYPE_DIAMOND:
+        case CARD_TYPE_CLUB:
+            sprintf(buf, "%s %s", types[card->type], indices[card->index]);
+            printf("%13s", buf);
+            break;
+        case CARD_TYPE_JOKER:
+            printf("%13s", jokers[card->index]);
+            break;
+        default:
+            break;
+    }
+}
+
 int main(void)
 {
     int i, k;
@@ -61,7 +95,7 @@ int main(void)
     for (i = 0; i < 3; i++) {
         printf("Player %d\n", i + 1);
         for (k = 0; k < 17; k++) {
-            printf("%13s", card++->name);
+            draw_card(card++);
             if (k && (k + 1) % 6 == 0) {
                 printf("\n");
             }
@@ -71,8 +105,9 @@ int main(void)
 
     printf("Remains\n");
     for (; card != &deck.cards[sizeof(deck.cards) / sizeof(deck.cards[0])]; card++) {
-        printf("%13s", card->name);
+        draw_card(card);
     }
+    printf("\n");
 
     return 0;
 }
